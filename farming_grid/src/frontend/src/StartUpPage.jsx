e343c62e87b4afa020851e3ca2c4c5fd27f70af2
hgomeUIC <hgome@uic.edu>
2025-04-05 16:32:57 -0500

    Added taiwincss for better style

--- farming_grid/src/frontend/src/StartUpPage.jsx
+++ farming_grid/src/frontend/src/StartUpPage.jsx
@@ -5,21 +5,17 @@ const StartUpPage = () => {
 	const background = "/img/13.jpg"; // Correct path for assets in the public folder
   
 	return (
-	  <div
-		style={{
-		  backgroundImage: `url(${background})`,
-		  backgroundSize: 'cover', // Ensures the image covers the entire container
-    	backgroundPosition: 'center', // Centers the image
-    	backgroundAttachment: 'fixed', // Keeps the background fixed during scrolling
-    	height: '100vh', // Full viewport height
-    	width: '100%', // Full width
-		margin: 0,          // Remove any default margin
-    padding: 0,         // Remove any default padding
-    border: 'none',     // Ensure no border is applied
-		  
-		}}
-	  >
-		<h1 style={{textAlign: 'center', fontSize : '4rem'}}>Welcome to My Page</h1>
+		<div style={{ backgroundImage: `url(${background})`,
+		backgroundSize: 'cover', // Ensure the image covers the entire element
+		backgroundPosition: 'center', // Center the image
+		backgroundAttachment: 'fixed', // Optional: Keep the background fixed during scroll
+		height: '100vh', // Make the element take up the full viewport height
+		width: '100%', }}>
+			<div>
+				<h1 style={{textAlign: 'center', fontSize : '4rem'}}>Welcome to My Page</h1>
+
+
+			</div>
 	  </div>
 	);
   };
