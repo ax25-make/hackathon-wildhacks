6b90865c32f4d946eb8ff7f7581fabd7d16261c6
hgomeUIC <hgome@uic.edu>
2025-04-05 14:51:27 -0500

    Added startup

--- farming_grid/src/frontend/src/StartUpPage.jsx
+++ farming_grid/src/frontend/src/StartUpPage.jsx
@@ -1,9 +1,20 @@
 import { StrictMode } from 'react'
+import "./StartUpPage.css"
 // import './LandingPage.css'
-
-function StartUpPage(){
+const StartUpPage = () => {
+	const background = "/img/images.jpeg"; // Correct path for assets in the public folder
+  
 	return (
-		<div><p>Hello</p></div>
-
-	)
-} export default StartUpPage;
+	  <div
+		style={{
+		  backgroundImage: `url(${background})`,
+		  backgroundSize: 'cover',
+		  backgroundPosition: 'center',
+		  height: '200vh',
+		}}
+	  >
+		<h1>Welcome to My Page</h1>
+	  </div>
+	);
+  };
+export default StartUpPage;
