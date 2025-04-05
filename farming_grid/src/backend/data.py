bfa9084dc2cae23aef50c637b5776a1522053acf
IsaacIsBetter <Isaac.contactg@gmail.com>
2025-04-05 17:10:45 -0500

    Update data.py

--- farming_grid/src/backend/data.py
+++ farming_grid/src/backend/data.py
@@ -22,5 +22,48 @@ PLANT_DATA = {
         "water_requirement": range(20, 31),
         "growth_cycle": (90, 120),
         "symbol": 'C'
-    }
+    },
+    "Sugarcane": {
+        "fertilizer_requirement": "high",
+        "water_requirement": range(30, 50),
+        "growth_cycle": (240, 300),
+        "symbol": 'S'
+    },
+    "Tobacco": {
+        "fertilizer_requirement": "moderate",
+        "water_requirement": range(20, 35),
+        "growth_cycle": (100, 150),
+        "symbol": 'T'
+    },
+    "Coffee": {
+        "fertilizer_requirement": "high",
+        "water_requirement": range(25, 40),
+        "growth_cycle": (180, 300),
+        "symbol": 'F'
+    },
+    "Bananas": {
+        "fertilizer_requirement": "high",
+        "water_requirement": range(30, 50),
+        "growth_cycle": (180, 300),
+        "symbol": 'N'
+    },
+    "Pineapple": {
+        "fertilizer_requirement": "moderate",
+        "water_requirement": range(25, 40),
+        "growth_cycle": (180, 240),
+        "symbol": 'I'
+    },
+    "Soybeans": {
+        "fertilizer_requirement": "moderate",
+        "water_requirement": range(20, 30),
+        "growth_cycle": (120, 180),
+        "symbol": 'Y'
+    },
+    "Rice": {
+        "fertilizer_requirement": "moderate",
+        "water_requirement": range(25, 35),
+        "growth_cycle": (120, 180),
+        "symbol": 'R'
+    },
+
 }
