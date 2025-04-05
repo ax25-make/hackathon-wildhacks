7a1977e401fcac8c6defd485b5588f20701c73bc
hgomeUIC <hgome@uic.edu>
2025-04-05 17:44:16 -0500

    Added new page for instructions

---
+++ farming_grid/src/frontend/src/Instruction.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+// import './LandingPage.css'
+const Instructions = () => {
+    return (
+		<div>
+			<h1 style = {{textAlign: 'center', fontSize : '3rem'}}>Instuctions</h1>
+		</div>
+	)
+ };
+export default Instructions;
